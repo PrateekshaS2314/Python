2bf8013b9452ffabc709a69a83673ca16c25dd87
PrateekshaS2314 <prateeksha242@gmail.com>
2025-03-07 09:58:24 +0530

    Create Program4.py

---
+++ Lab-6/Program4.py
@@ -0,0 +1,10 @@
+# Create a DataFrame from a dictionary
+import pandas as pd
+data = {
+"Name": ["Ram", "Robert", "Rahim"],
+"Age": [25, 30, 35],
+"City": ["Ayodya", "Chennai", "Delhi"],
+}
+df = pd.DataFrame(data)
+print(df)
+print(type(df))
