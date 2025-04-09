e9279c01304f4010131dcfce7ab8993bbe1c818d
PrateekshaS2314 <prateeksha242@gmail.com>
2025-04-10 00:14:47 +0530

    Update Activity3b.py

--- Lab-9/Lab Record/Activity3b.py
+++ Lab-9/Lab Record/Activity3b.py
@@ -3,10 +3,10 @@ def merge_dict(dict1, dict2):
     merged_dict.update(dict2)
     return merged_dict
 
-print("Enter 1st dictionary (format: key1:value1, key2:value2):")
+print("Enter first dictionary (format: key1:value1, key2:value2):")
 dict1_input = input()
 
-print("Enter 2nd dictionary (format: key1:value1, key2:value2):")
+print("Enter second dictionary (format: key1:value1, key2:value2):")
 dict2_input = input()
 
 dict1 = {key: value for key, value in (item.split(":") for item in dict1_input.split(","))}
